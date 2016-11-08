0487dbb8b374d685a0fc5c562058b6cbe6a2fea8
Nat York <natyork@gmail.com>
2016-11-08 23:46:51 +0000

    Implement ex 6-7

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -10,6 +10,10 @@ Store.create(name: 'Surrey', annual_revenue: 224000, mens_apparel: false, womens
 Store.create(name: 'Whistler', annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
 Store.create(name: 'Yaletown', annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
 
+@store4 = Store.find(4)
+@store5 = Store.find(5)
+@store6 = Store.find(6)
+
 @mens_stores = Store.where(mens_apparel: true)
 @mens_stores.each do |store|
   puts "#{store.name} has an annual revenue of #{store.annual_revenue}"
--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -8,14 +8,22 @@ require_relative './exercise_5'
 puts "Exercise 6"
 puts "----------"
 
-# Your code goes here ...
-# ### Exercise 6: One-to-many association
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 70)
+@store1.employees.create(first_name: "Khurram", last_name: "Virani2", hourly_rate: 50)
+@store1.employees.create(first_name: "Khurram", last_name: "Virani3", hourly_rate: 20)
+@store2.employees.create(first_name: "Khurram", last_name: "Virani4", hourly_rate: 60)
+@store2.employees.create(first_name: "Khurram", last_name: "Virani5", hourly_rate: 60)
+@store2.employees.create(first_name: "Khurram", last_name: "Virani6", hourly_rate: 60)
+@store4.employees.create(first_name: "Khurram", last_name: "Virani7", hourly_rate: 60)
+@store4.employees.create(first_name: "Khurram", last_name: "Virani8", hourly_rate: 60)
+@store4.employees.create(first_name: "Khurram", last_name: "Virani9", hourly_rate: 60)
+@store5.employees.create(first_name: "Khurram", last_name: "Virani10", hourly_rate: 60)
+@store5.employees.create(first_name: "Khurram", last_name: "Virani11", hourly_rate: 60)
+@store5.employees.create(first_name: "Khurram", last_name: "Virani12", hourly_rate: 60)
+@store6.employees.create(first_name: "Khurram", last_name: "Virani13", hourly_rate: 60)
+@store6.employees.create(first_name: "Khurram", last_name: "Virani14", hourly_rate: 60)
+@store6.employees.create(first_name: "Khurram", last_name: "Virani15", hourly_rate: 60)
 
-# We haven't used the Employee class (and employees table) at all yet. If you look at this table's column structure, you'll find that it has a `store_id` (integer) column. This is a column that identifies which store each employee belongs to. It points to the `id` (integer) column of their store.
 
-# Let's tell Active Record that these two tables are in fact related via the `store_id` column.
 
-# 1. Add the following code _directly_ inside the Store model (class): `has_many :employees`
-# 2. Add the following code directly inside the Employee model (class): `belongs_to :store`
-# 3. Add some data into employees. Here's an example of one (note how it differs from how you create stores): `@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)`
-# 4. Go ahead and create some more employees using the create method. You can do this by making multiple calls to create (like you have before.) No need to assign the employees to variables though. Create them through the `@store#` instance variables that you defined in previous exercises. Create a bunch under `@store1` (Burnaby) and `@store2` (Richmond). Eg: `@store1.employees.create(...)`.
+
