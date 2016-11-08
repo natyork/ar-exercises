9deddfaaf57f9b060eae621a5a5dfdd6efae6c51
Nat York <natyork@gmail.com>
2016-11-08 21:30:22 +0000

    Impement ex 1-5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -7,4 +7,11 @@ require_relative './exercise_4'
 puts "Exercise 5"
 puts "----------"
 
-# Your code goes here ...
+@total_revenue = Store.sum(:annual_revenue)
+puts "The total revenue from all stores is $#{@total_revenue}."
+
+@average_revenue = Store.average(:annual_revenue)
+puts "The average revenue from all stores is $#{@average_revenue}."
+
+@rich_stores = Store.where("annual_revenue > ?", 1000000).count
+puts "There are #{@rich_stores} rich stores in our empire."
